9ab7d7f256530ad6df0d75808068021004b3b1a8
ExceLLent <zerkc_97@mail.ru>
2018-02-22 15:58:41 +0300

    Add error handling

--- source/main.c
+++ source/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -10,30 +12,76 @@
 
 int main(int argc, char *argv[])
 {
-	char *address = argv[1];
-
+	char *address, *prog_name = argv[0];
 	int network_socket;
-	network_socket = socket(AF_INET, SOCK_STREAM, 0);
-
+	int conversion_status;
 	struct sockaddr_in server_address;
-	memset(&server_address, 0, sizeof(server_address));
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(22);
-	inet_pton(AF_INET, address, &server_address.sin_addr);
-
-	printf("Connecting to %s\n", address);
-	connect(network_socket, (struct sockaddr *) &server_address,
-		 				sizeof(server_address));
-
 	char request[] = "SSH-2.0-OpenSSH_7.1\r\n";
 	char server_response[4096];
 
-	send(network_socket, request, sizeof(request), 0);
-	recv(network_socket, &server_response, sizeof(server_response), 0);
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s <IPv4 address>\n", prog_name);
+		exit(EXIT_FAILURE);
+	}
+
+	address = argv[1];
+
+	if ((network_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		fprintf(stderr, "%s: cannot create socket: %s",
+						 prog_name, strerror(errno));
+		close(network_socket);
+		exit(EXIT_FAILURE);
+	}
+
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(22);
+	if ((conversion_status = inet_pton(AF_INET, address,
+					 &server_address.sin_addr)) == -1) {
+		fprintf(stderr, "%s: address conversion failed: %s",
+						 prog_name, strerror(errno));
+		close(network_socket);
+		exit(EXIT_FAILURE);
+	} else if (!conversion_status) {
+		fprintf(stderr, "%s: input isn't a valid IP address\n",
+								 prog_name);
+		close(network_socket);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Connecting to %s\n", address);
+	if (connect(network_socket, (struct sockaddr *) &server_address,
+		 			sizeof(server_address)) == -1) {
+		fprintf(stderr, "%s: connection failed: %s",
+						 prog_name, strerror(errno));
+		close(network_socket);
+		exit(EXIT_FAILURE);
+	}
+
+	if (send(network_socket, request, sizeof(request), 0) == -1) {
+		fprintf(stderr, "%s: cannot send data: %s",
+						 prog_name, strerror(errno));
+		close(network_socket);
+		exit(EXIT_FAILURE);		
+	}
+	if (recv(network_socket, &server_response, 
+					sizeof(server_response), 0) == -1) {
+		fprintf(stderr, "%s: cannot receive data: %s",
+						 prog_name, strerror(errno));
+		close(network_socket);
+		exit(EXIT_FAILURE);
+	}
+	if (recv(network_socket, &server_response, 
+					sizeof(server_response), 0) == 0) {
+		fprintf(stderr, "%s: the server has closed the connection",
+								 prog_name);
+		close(network_socket);
+		exit(EXIT_FAILURE);
+	}
 	printf("The server sent the data:\n%s", server_response);
 
 	shutdown(network_socket, SHUT_RDWR);
 	close(network_socket);
 
-	return 0;
+	return EXIT_SUCCESS;
 }
