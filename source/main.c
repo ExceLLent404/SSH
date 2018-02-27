defce13f3c50ba6476c24ebec834974d425cdb40
ExceLLent <zerkc_97@mail.ru>
2018-02-27 13:56:55 +0300

    Use the symbolic constant for the SSH port

--- source/main.c
+++ source/main.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
 
 	memset(&server_address, 0, sizeof(server_address));
 	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(22);
+	server_address.sin_port = htons(SSH_PORT);
 	if ((conversion_status = inet_pton(AF_INET, address,
 					 &server_address.sin_addr)) == -1) {
 		fprintf(stderr, "%s: address conversion failed: %s",
