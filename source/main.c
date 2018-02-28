f1d3761927f2a4c64409a2ce83d7a868a7b8a3e1
ExceLLent <zerkc_97@mail.ru>
2018-02-28 14:59:05 +0300

    Split the main routine
    The code that was used to initialize the connection and for Protocol
    Version Exchange was isolated to the corresponding functions. Thus, the
    size of main was reduced.

--- source/main.c
+++ source/main.c
@@ -11,21 +11,12 @@
 #define SSH_PORT 22
 #define MAX_BUF_SIZE 100
 
-int main(int argc, char *argv[])
+/* Returns a new socket descriptor */
+int initialize_connection(char *address, char *prog_name)
 {
-	char *address, *prog_name = argv[0];
 	int network_socket;
-	int conversion_status, numbytes;
+	int conversion_status;
 	struct sockaddr_in server_address;
-	char request[] = "SSH-2.0-OpenSSH_7.1\r\n";
-	char server_response[MAX_BUF_SIZE];
-
-	if (argc < 2) {
-		fprintf(stderr, "Usage: %s <IPv4 address>\n", prog_name);
-		exit(EXIT_FAILURE);
-	}
-
-	address = argv[1];
 
 	if ((network_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
 		fprintf(stderr, "%s: cannot create socket: %s",
@@ -59,9 +50,20 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	if (send(network_socket, request, strlen(request), 0) == -1) {
-		fprintf(stderr, "%s: cannot send data: %s",
-						 prog_name, strerror(errno));
+	return network_socket;
+}
+
+void protocol_version_exchange(int network_socket, char *prog_name)
+{
+	char identification_string[] = "SSH-2.0-OpenSSH_7.1\r\n";
+	char server_response[MAX_BUF_SIZE];
+	int numbytes;
+
+	printf("Sending the identification string: %s", identification_string);
+	if (send(network_socket, identification_string, 
+				strlen(identification_string), 0) == -1) {
+		fprintf(stderr, "%s: cannot send the identification "
+				"string: %s", prog_name, strerror(errno));
 		close(network_socket);
 		exit(EXIT_FAILURE);		
 	}
@@ -80,6 +82,23 @@ int main(int argc, char *argv[])
 	}
 	server_response[numbytes] = '\0';
 	printf("The server sent the data:\n%s", server_response);
+}
+
+int main(int argc, char *argv[])
+{
+	char *address, *prog_name = argv[0];
+	int network_socket;
+
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s <IPv4 address>\n", prog_name);
+		exit(EXIT_FAILURE);
+	}
+
+	address = argv[1];
+
+	network_socket = initialize_connection(address, prog_name);
+
+	protocol_version_exchange(network_socket, prog_name);
 
 	shutdown(network_socket, SHUT_RDWR);
 	close(network_socket);
