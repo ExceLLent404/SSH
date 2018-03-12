da1104131872249cbb722e42cedbcbf3f0170b76
ExceLLent <zerkc_97@mail.ru>
2018-03-12 07:57:25 +0300

    Add utility functions

--- source/main.c
+++ source/main.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <time.h>
+#include <stdint.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -82,6 +84,61 @@ void protocol_version_exchange(int network_socket, char *prog_name)
 	}
 	server_response[numbytes] = '\0';
 	printf("The server sent the data:\n%s", server_response);
+}
+
+void set_random_bytes(uint8_t *destination, int numbytes)
+{
+	int i, j, remainder, step_size = sizeof(uint32_t);
+	int step = numbytes / step_size;
+	uint32_t random_value;
+	uint8_t *ptr = (uint8_t *) &random_value;
+
+	srand(time(NULL));
+	for (i = 0; i < step; ++i) {
+		random_value = rand();
+		*(uint32_t *) (destination + i * step_size) = random_value;
+	}
+	if (remainder = numbytes % sizeof(uint32_t))
+		random_value = rand();
+	for (j = 0; j < remainder; ++j)
+		destination[j + i * step_size] = ptr[j];
+}
+
+size_t get_packet_size(size_t payload_size, size_t cipher_block_size, 
+								int mac_length)
+{
+	int i, piece_size = (cipher_block_size > 8) ? cipher_block_size : 8;
+	size_t packet_size, padding_length = 4;
+
+	packet_size = sizeof(uint32_t) + sizeof(uint8_t) + payload_size;
+	if ((packet_size + padding_length) % piece_size)
+		padding_length += piece_size - 
+				(packet_size + padding_length) % piece_size;
+	packet_size += padding_length + mac_length;
+
+	return packet_size;
+}
+
+void wrap_message(uint8_t *data_packet, size_t packet_size, 
+					size_t payload_size, int mac_length)
+{
+	int shift = 0;
+	uint32_t packet_length;
+	uint8_t padding_length;
+
+	packet_length = packet_size - sizeof(uint32_t) - mac_length;
+	padding_length = packet_length  - sizeof(uint8_t)- payload_size;
+
+	*(uint32_t *) (data_packet) = htonl(packet_length);
+	shift += sizeof(uint32_t);
+	data_packet[shift++] = padding_length;
+	shift += payload_size;
+	set_random_bytes(data_packet + shift, padding_length);
+	/*
+	shift += padding_length;
+	if (mac_length > 0)
+		There must be the MAC
+	*/
 }
 
 int main(int argc, char *argv[])
