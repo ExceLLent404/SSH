59cae091dc599a8a7dbd6525ba2b68a6a8f0c18d
ExceLLent <zerkc_97@mail.ru>
2018-04-20 13:28:20 +0300

    Edit error handling
    It is now easier to print an error message because of the addition of
    the corresponding function.

--- source/main.c
+++ source/main.c
@@ -13,16 +13,24 @@
 
 #define SSH_PORT 22
 
+char *prog_name;
+
+void print_error(const char *str)
+{
+	fprintf(stderr, "%s: %s", prog_name, str);
+	if (errno)
+		fprintf(stderr, ": %s", strerror(errno));
+}
+
 /* Returns a new socket descriptor */
-int initialize_connection(char *address, char *prog_name)
+int initialize_connection(char *address)
 {
 	int network_socket;
 	int conversion_status;
 	struct sockaddr_in server_address;
 
 	if ((network_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		fprintf(stderr, "%s: cannot create socket: %s",
-						 prog_name, strerror(errno));
+		print_error("cannot create socket");
 		close(network_socket);
 		exit(EXIT_FAILURE);
 	}
@@ -32,13 +40,11 @@ int initialize_connection(char *address, char *prog_name)
 	server_address.sin_port = htons(SSH_PORT);
 	if ((conversion_status = inet_pton(AF_INET, address,
 					 &server_address.sin_addr)) == -1) {
-		fprintf(stderr, "%s: address conversion failed: %s",
-						 prog_name, strerror(errno));
+		print_error("address conversion failed");
 		close(network_socket);
 		exit(EXIT_FAILURE);
 	} else if (!conversion_status) {
-		fprintf(stderr, "%s: input isn't a valid IP address\n",
-								 prog_name);
+		print_error("input isn't a valid IP address");
 		close(network_socket);
 		exit(EXIT_FAILURE);
 	}
@@ -46,8 +52,7 @@ int initialize_connection(char *address, char *prog_name)
 	printf("Connecting to %s\n", address);
 	if (connect(network_socket, (struct sockaddr *) &server_address,
 		 			sizeof(server_address)) == -1) {
-		fprintf(stderr, "%s: connection failed: %s",
-						 prog_name, strerror(errno));
+		print_error("connection failed");
 		close(network_socket);
 		exit(EXIT_FAILURE);
 	}
@@ -57,7 +62,7 @@ int initialize_connection(char *address, char *prog_name)
 
 #define MAX_BUF_SIZE 1024
 
-void protocol_version_exchange(int network_socket, char *prog_name)
+void protocol_version_exchange(int network_socket)
 {
 	char identification_string[] = "SSH-2.0-OpenSSH_7.1\r\n";
 	char server_response[MAX_BUF_SIZE];
@@ -66,21 +71,18 @@ void protocol_version_exchange(int network_socket, char *prog_name)
 	printf("Sending the identification string: %s", identification_string);
 	if (send(network_socket, identification_string, 
 				strlen(identification_string), 0) == -1) {
-		fprintf(stderr, "%s: cannot send the identification "
-				"string: %s", prog_name, strerror(errno));
+		print_error("cannot send the identification string");
 		close(network_socket);
 		exit(EXIT_FAILURE);		
 	}
 	if ((numbytes = recv(network_socket, server_response, 
 						MAX_BUF_SIZE - 1, 0)) == -1) {
-		fprintf(stderr, "%s: cannot receive data: %s",
-						 prog_name, strerror(errno));
+		print_error("cannot receive data");
 		close(network_socket);
 		exit(EXIT_FAILURE);
 	}
 	if (numbytes == 0) {
-		fprintf(stderr, "%s: the server has closed the connection",
-								 prog_name);
+		print_error("the server has closed the connection");
 		close(network_socket);
 		exit(EXIT_FAILURE);
 	}
@@ -189,7 +191,7 @@ void set_kexinit_msg(uint8_t *data, char *name_list[NAME_LIST_SIZE])
 }
 
 /* Algoritm Negotiation: the SSH_MSG_KEXINIT message exchange */
-void negotiate_algorithm(int network_socket, char *prog_name)
+void negotiate_algorithm(int network_socket)
 {
 	int numbytes, shift;
 	uint8_t server_response[MAX_BUF_SIZE];
@@ -217,15 +219,13 @@ void negotiate_algorithm(int network_socket, char *prog_name)
 	printf("Expecting the SSH_MSG_KEXINIT message from the server\n");
 	if ((numbytes = recv(network_socket, server_response, 
 						MAX_BUF_SIZE, 0)) == -1) {
-		fprintf(stderr, "%s: cannot receive the SSH_MSG_KEXINIT"
-				" message: %s", prog_name, strerror(errno));
+		print_error("cannot receive the SSH_MSG_KEXINIT message");
 		free(data_packet);
 		close(network_socket);
 		exit(EXIT_FAILURE);
 	}
 	if (numbytes == 0) {
-		fprintf(stderr, "%s: the server has closed the connection",
-								 prog_name);
+		print_error("the server has closed the connection");
 		free(data_packet);
 		close(network_socket);
 		exit(EXIT_FAILURE);
@@ -234,8 +234,7 @@ void negotiate_algorithm(int network_socket, char *prog_name)
 
 	printf("Sending the SSH_MSG_KEXINIT message to server\n");
 	if (send(network_socket, data_packet, packet_size, 0) == -1) {
-		fprintf(stderr, "%s: cannot send the SSH_MSG_KEXINIT "
-				"message: %s", prog_name, strerror(errno));
+		print_error("cannot send the SSH_MSG_KEXINIT message");
 		free(data_packet);
 		close(network_socket);
 		exit(EXIT_FAILURE);		
@@ -247,20 +246,22 @@ void negotiate_algorithm(int network_socket, char *prog_name)
 
 int main(int argc, char *argv[])
 {
-	char *address, *prog_name = argv[0];
+	char *address;
 	int network_socket;
 
+	prog_name = argv[0];
+
 	if (argc < 2) {
-		fprintf(stderr, "Usage: %s <IPv4 address>\n", prog_name);
+		print_error("Usage: %s <IPv4 address>\n");
 		exit(EXIT_FAILURE);
 	}
 
 	address = argv[1];
 
-	network_socket = initialize_connection(address, prog_name);
+	network_socket = initialize_connection(address);
 
-	protocol_version_exchange(network_socket, prog_name);
-	negotiate_algorithm(network_socket, prog_name);
+	protocol_version_exchange(network_socket);
+	negotiate_algorithm(network_socket);
 
 	shutdown(network_socket, SHUT_RDWR);
 	close(network_socket);
